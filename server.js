addd3f0c2f33fde32193f40ae953fb87f68130e1
chandchand1514-blip <chandchand1514@gmail.com>
2026-08-29 04:42:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const multer = require('multer');
 const sharp = require('sharp');
 const path = require('path');
 const fs = require('fs');
+const archiver = require('archiver');
 
 const app = express();
 const upload = multer({ dest: 'uploads/' }); 
@@ -23,15 +24,32 @@ app.post('/build', upload.single('appLogo'), async (req, res) => {
 
         console.log(`Processing App: ${appName} for URL: ${appUrl}`);
 
+        // 1. Logo save karein
         const logoOutputPath = path.join(__dirname, 'base_template', 'app_logo.png');
         await sharp(logoFile.path)
             .resize(192, 192)
             .toFile(logoOutputPath);
 
+        // 2. URL save karein
         const urlOutputPath = path.join(__dirname, 'base_template', 'url.txt');
         fs.writeFileSync(urlOutputPath, appUrl);
 
-        res.send(`Badhai ho! ${appName} ka URL aur Logo template ke andar successfully inject ho gaye hain.`);
+        // 3. Template folder ko ZIP karke user ko download dena
+        const zipName = `${appName.replace(/\s+/g, '_')}_App.zip`;
+        const zipPath = path.join(__dirname, 'uploads', zipName);
+        
+        const output = fs.createWriteStream(zipPath);
+        const archive = archiver('archiver', { zlib: { level: 9 } });
+
+        output.on('close', () => {
+            res.download(zipPath, zipName, (err) => {
+                if (err) console.error(err);
+            });
+        });
+
+        archive.pipe(output);
+        archive.directory(path.join(__dirname, 'base_template'), false);
+        archive.finalize();
 
     } catch (err) {
         console.error(err);
